dd7e8d10a16248eabaf89f4bfa90d0e8ecdb0335
Vinayak Phal <vphal@paypal.com>
2020-07-18 15:24:50 +0530

    Added styles for login page

---
+++ src/app/pages/Dashboard/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Dashboard extends React.PureComponent {
+
+    render(){
+        return(
+            <div>
+                <div className="row">
+                    <div className="col-lg-6 col-md-6 col-sm-6 grid-margin stretch-card">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="row">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Dashboard;
